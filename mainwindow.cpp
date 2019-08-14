9a11b1b7cfd31f312c530c836641b9e638b981fd
Andrey <andreysansab@gmail.com>
2019-08-13 23:33:02 -0600

    Sockets en cliente

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,23 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
+
+#include <QLabel>
+#include <QGraphicsItem>
+#include <stdio.h>
+#include <unistd.h>
+#include <chrono>
+#include <thread>
+#include <QDateTime>
+#include <QTimer>
+#include <QMatrix>
+#include <QTransform>
+#include <QWidget>
+#include <QColor>
+#include <string>
+
+
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +29,84 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_SendButton_clicked()
+{
+    cout << ui->Insert->text().toStdString() <<endl;
+    sendJSON("INSERT",ui->Insert->text().toStdString());
+}
+
+
+int MainWindow::sendJSON(string KEY, string data){
+    char* str;
+    int fd, numbytes;
+    struct sockaddr_in client;
+
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    char sendBuff[MAXDATASIZE];
+    char recvBuff[MAXDATASIZE];
+
+    struct hostent *he;
+
+    if (fd < 0)
+    {
+        printf("Error : Could not create socket\n");
+        return 1;
+    }
+    else
+    {
+        client.sin_family = AF_INET;
+        client.sin_port = htons(PORT);
+        client.sin_addr.s_addr = inet_addr("127.0.0.1"); //192.168.100.6
+        memset(client.sin_zero, '\0', sizeof(client.sin_zero));
+    }
+
+    if (::connect(fd, (const struct sockaddr *)&client, sizeof(client)) < 0)
+    {
+        printf("ERROR connecting to server\n");
+        return 1;
+    }
+
+
+    json_object *jobj = json_object_new_object();
+
+    json_object *jstring = json_object_new_string(data.c_str());
+
+    json_object_object_add(jobj,KEY.c_str(), jstring);
+
+
+    if (strcpy(sendBuff, json_object_to_json_string(jobj)) == NULL) {
+        printf("ERROR strcpy()");
+        exit(-1);
+    }
+
+    if (write(fd, sendBuff, strlen(sendBuff)) == -1)
+    {
+        printf("ERROR write()");
+        exit(-1);
+    }
+
+    if ((numbytes=recv(fd,recvBuff,MAXDATASIZE,0)) < 0){
+
+        printf("Error en recv() \n");
+        exit(-1);
+    }
+
+    struct json_object *tempRecibido;
+    json_object *parsed_jsonRecibido = json_tokener_parse(recvBuff);
+    json_object_object_get_ex(parsed_jsonRecibido, "RECIBIDO", &tempRecibido);
+    if (json_object_get_int(tempRecibido) != 0){
+        cout << json_object_get_string(tempRecibido) << endl;
+    }
+
+
+
+    ///Se limpian los Buffers
+    memset(recvBuff, 0, MAXDATASIZE);
+    memset(sendBuff, 0, MAXDATASIZE);
+
+    ::close(fd);
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,35 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <cstdlib>
+#include <strings.h>
+#include <unistd.h>
+#include <cstring>
+#include <iostream>
+#include <json-c/json.h>
+#include <json-c/debug.h>
+#include <json-c/json_object.h>
+#include <QMessageBox>
+
+
+#define PORT 3550
+#define MAXDATASIZE 1000
+
+class Thread;
+class QTimer;
+
+using namespace std;
+
+
 
 namespace Ui {
 class MainWindow;
@@ -14,6 +43,10 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    int sendJSON(string KEY, string data);
+
+private slots:
+    void on_SendButton_clicked();
 
 private:
     Ui::MainWindow *ui;
